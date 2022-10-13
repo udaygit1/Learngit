738b71005454f0b83c5a92bc521e242b83f17ce6
udaygit1 <115709730+udaygit1@users.noreply.github.com>
2022-10-14 04:31:43 +0530

    Update Check.java

--- src/Check.java
+++ src/Check.java
@@ -2,6 +2,6 @@ public class Check {
     public static void main(String[]args)
     {
         System.out.println("class java2");
-           //modification is done.
+           //modification is done in done in the git master.
     }
 }
